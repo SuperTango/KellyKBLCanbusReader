85311563b1d24316571b42beb497d571673a7c62
Alex.Tang <altitude@proofpoint.com>
2011-10-03 00:24:17 -0700

    Update calculate to understand different tangolog files

--- src/data_parsers/calculate.pl
+++ src/data_parsers/calculate.pl
@@ -2,6 +2,11 @@
 use strict;
 use warnings;
 
+my $headers = {
+    v1 => [ 'current_millis','diff_millis','count','iterations','date','time','speed_gps','speed_rpm','lat','lon','heading','distance_gps','distance_rpm','batt_voltage','batt_current_reading','batt_current','motor_current','motor_voltage','motor_watts','wh/m_gps','wh/m_rpm','m/kwh_gps','m/kwh_rpm','motor_temp_calc','motor_thermistor_reading','brake_a/d','tps_a/d','controller_power','5v_power','b+','ia','ib','ic','va','vb','vc','pwm','enable_motor_rotation','motor_temp','controller_temp','high_mosfet','low_mosfet','rpm_high','rpm_low','current_%','error_high','error_low'],
+    v2 => [ 'current_millis','diff_millis','count','iterations','date','time','fix_age','speed_gps','speed_rpm','lat','lon','altitude','heading','distance_gps','distance_rpm','batt_voltage','batt_current_reading','batt_current','motor_current','motor_voltage','motor_watts','wh/m_gps','wh/m_rpm','m/kwh_gps','m/kwh_rpm','motor_temp_calc','motor_thermistor_reading','brake_a/d','tps_a/d','controller_power','5v_power','b+','ia','ib','ic','va','vb','vc','pwm','enable_motor_rotation','motor_temp','controller_temp','high_mosfet','low_mosfet','rpm_high','rpm_low','current_%','error_high','error_low'],
+    v3 => [ 'current_millis','diff_millis','count','iterations','date','time','fix_age','speed_gps','speed_rpm','lat','lon','altitude','heading','failed_cs','distance_gps','distance_rpm','batt_voltage','batt_current_reading','batt_current','motor_current','motor_voltage','motor_watts','wh/m_gps','wh/m_rpm','m/kwh_gps','m/kwh_rpm','motor_temp_calc','motor_thermistor_reading','brake_a/d','tps_a/d','controller_power','5v_power','b+','ia','ib','ic','va','vb','vc','pwm','enable_motor_rotation','motor_temp','controller_temp','high_mosfet','low_mosfet','rpm_high','rpm_low','current_%','error_high','error_low'],
+}
 my $diffCol = 1;
 my $distanceCol = 11;
 my $battVoltCol = 13;
@@ -12,7 +17,12 @@ open ( my $fh, $file ) || die;
 my $totalWh = 0;
 my $totalTime = 0;
 my $totalDistance = 0;
+my $type = 'v1';
+
 while ( my $line = <$fh> ) {
+    if ( $line =~ /LOGFMT (\d+)/ ) {
+        $type = 'v' . $1;
+    }
     chomp $line;
     next if ( $line =~ /^\s*#/ );
     next if ( $line =~ /^\s*$/ );
